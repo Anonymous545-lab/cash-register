5630491f4cb5b1fd3dbbc1ba0abc3a0111a83617
Anonymous545-lab <briadonfielies123@gmail.com>
2025-01-26 15:59:08 +0200

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import './NavBar.css';
 const NavBar = ({ setTab, userName }) => {
     return (
         <nav className="nav-ribbon">
-            <div className="user-name">Welcome, {userName}</div>
+            <div className="user-name">Welcome, may you have a special day</div>
             <ul className="nav-list">
                 <li className="nav-item" onClick={() => setTab('cash_register')}>Cash Register</li>
                 <li className="nav-item" onClick={() => setTab('transaction_logs')}>Transaction Logs</li>
